2861160695472904542472a6bfcd1e572fad7738
czs208112 <czs208112@gmail.com>
2017-04-27 09:43:43 +0800

    MapreduceDemo WordCount

---
+++ src/main/java/com/summit/Utils.java
@@ -0,0 +1,27 @@
+package com.summit;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Utils {
+	
+	/**
+	 * 根据配置文件key值获取value值
+	 * @param key
+	 * @return
+	 */
+	public static String getProp(String key) {
+		// 读取配置文件
+		Properties pro = new Properties();
+		InputStream in = Utils.class.getClassLoader().getResourceAsStream("paramConfig.properties");
+		String valStr = "";
+		try {
+			pro.load(in);
+			valStr = pro.getProperty(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return valStr;
+	}
+}
